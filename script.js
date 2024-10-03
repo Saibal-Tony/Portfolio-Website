ff888181eddfb1e36b9f73ae35a7348660be8b24
Saibal Bera <107796954+Saibal-Tony@users.noreply.github.com>
2024-10-03 21:02:22 +0530

    changes

--- script.js
+++ script.js
@@ -19,4 +19,30 @@ var tablinks = document.getElementsByClassName("tab-links");
             item.classList.add('active');
         });
     });
-    
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+// ------------------------------contact-----------------------------
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxRBMkAxCXBYGNKPSHwWurhO1vrBBAU9oimltGOzjqy-HHG1AAb2mMnsxiWPLHlVhY9Ng/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message Sent Successfully!"
+        setTimeout(function(){
+            msg.innerHTML =""
+        },5000);
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
